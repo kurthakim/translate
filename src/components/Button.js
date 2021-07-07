9fa84fc66b04ceb95ac89497b1595fc5488d4061
Kurt Hakim <kurthakim@gmail.com>
2021-07-08 01:02:28 +0700

    add context Provider and Customer

---
+++ src/components/Button.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen';
+  }
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => (
+          <button className={`ui button ${color}`}>
+            <LanguageContext.Consumer>
+              {(value) => this.renderSubmit(value)}
+            </LanguageContext.Consumer>
+          </button>
+        )}
+      </ColorContext.Consumer>
+    );
+  }
+}
+
+export default Button;
